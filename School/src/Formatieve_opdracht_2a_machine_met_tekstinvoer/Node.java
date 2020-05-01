6ebe62961fe8bd90a52f24e7c79827f4c7fe5f6b
wail0152 <60653485+wail0152@users.noreply.github.com>
2020-05-01 22:02:55 +0200

    - Added full functionality except no connection case

--- School/src/Formatieve_opdracht_2a_machine_met_tekstinvoer/Node.java
+++ School/src/Formatieve_opdracht_2a_machine_met_tekstinvoer/Node.java
@@ -1,29 +1,38 @@
 package Formatieve_opdracht_2a_machine_met_tekstinvoer;
 
+import java.util.HashMap;
+
 public class Node {
     
     private Node A;
     private Node B;
+    private HashMap<String, Node> map = new HashMap<String, Node>();
+    private String nodeName;
+
+    public Node(String nodeName)
+    {
+        this.nodeName = nodeName;
+    }
 
     public void setNode(Node A, Node B)
     {
         this.A = A;
         this.B = B;
+        initHashMap();
+    }
+
+    private void initHashMap()
+    {
+        map.put("A", A);
+        map.put("B", B);
     }
 
     public void giveInput(String input)
-    {
-        System.out.println(input.substring(0, 1));
-        switch(input.substring(0, 1))
-        {
-            case "A":
-                A.giveInput(input.substring(1, -1));
-                break;
-
-            case "B":
-                B.giveInput(input.substring(1, -1));
-                break;
-        } 
+    {        
+        if (input.length() > 1)
+            map.get(input.substring(0, 1)).giveInput(input.substring(1, input.length()));
+        else
+            System.out.println("Ended at: " + map.get(input.substring(0, 1)).nodeName);
     }
 
 }
--- School/src/Formatieve_opdracht_2a_machine_met_tekstinvoer/main.java
+++ School/src/Formatieve_opdracht_2a_machine_met_tekstinvoer/main.java
@@ -3,17 +3,17 @@ package Formatieve_opdracht_2a_machine_met_tekstinvoer;
 public class main {
 
     public static void main(String[] args) {
-        Node s0 = new Node();
-        Node s1 = new Node();
-        Node s2 = new Node();
-        Node s3 = new Node();
+        Node s0 = new Node("s0");
+        Node s1 = new Node("s1");
+        Node s2 = new Node("s2");
+        Node s3 = new Node("s3");
 
         s0.setNode(s2, s1);
         s1.setNode(s1, s2);
         s2.setNode(s2, s3);
         s3.setNode(s3, s0);
 
-        s1.giveInput("BAAB");
+        s0.giveInput("BAAB");
     }
     
 }
